043cd0a5d0e6897891f148529fd859742951abe7
Benjamin Manford <benjaminmanford@gmail.com>
2019-04-22 08:08:22 +0100

    Run formater

--- lib/hubtel_ussd_phoenix_example_web/controllers/page_controller.ex
+++ lib/hubtel_ussd_phoenix_example_web/controllers/page_controller.ex
@@ -1,50 +1,54 @@
 defmodule HubtelUssdPhoenixExampleWeb.PageController do
-  	use HubtelUssdPhoenixExampleWeb, :controller
+  use HubtelUssdPhoenixExampleWeb, :controller
 
-  	def index(conn, _params) do
-	    # read raw POST data from Hubtel
-	    {:ok, body, _conn} = Plug.Conn.read_body(conn)
+  def index(conn, _params) do
+    # read raw POST data from Hubtel
+    {:ok, body, _conn} = Plug.Conn.read_body(conn)
 
-	    ussdRequest = Jason.decode!(body)
+    ussdRequest = Jason.decode!(body)
 
+    # check if ussdRequest != nil
+    if ussdRequest do
+      # get the request type
+      case ussdRequest["Type"] do
+        # initial request
+        "Initiation" ->
+          ussdResponse = %{
+            Message:
+              "Welcome to AquaSafari (" <> ussdRequest["Mobile"] <> ")\n1. Continue\n2. Cancel",
+            ClientState: "continue-menu",
+            Type: "Response"
+          }
 
-    	# check if ussdRequest != nil
-    	if ussdRequest do
-      		# get the request type
-      		case ussdRequest["Type"] do
-        		# initial request
-			        "Initiation" ->
-			          	ussdResponse = %{
-				            Message: "Welcome to AquaSafari (" <> ussdRequest["Mobile"] <> ")\n1. Continue\n2. Cancel",
-				            ClientState: "continue-menu",
-				            Type: "Response"
-			          	}
+          send_resp(conn, :ok, Jason.encode!(ussdResponse))
 
-			          	send_resp(conn, :ok, Jason.encode!(ussdResponse))
+        # continuing request
+        "Response" ->
+          # get request sequence
+          case ussdRequest["Sequence"] do
+            2 ->
+              # get the clientState
+              case ussdRequest["ClientState"] do
+                # if clientState == "continue-menu"
+                "continue-menu" ->
+                  # check if user input is 1 from home menu
+                  if ussdRequest["Message"] == "1" do
+                    ussdResponse = %{
+                      Message: "Select an option\n1. Book a Room\n2. Contact Us",
+                      Type: "Release"
+                    }
 
-			        # continuing request
-			        "Response" ->
-			          	# get request sequence
-			          	case ussdRequest["Sequence"] do
-				            2 ->
-			              	# get the clientState
-			              	case ussdRequest["ClientState"] do
-			                	# if clientState == "continue-menu"
-		                		"continue-menu" ->
-		                  		# check if user input is 1 from home menu
-		                  		if ussdRequest["Message"] == "1" do
-		                    		ussdResponse = %{Message: "Select an option\n1. Book a Room\n2. Contact Us", Type: "Release"}
-		                    		send_resp(conn, :ok, Jason.encode!(ussdResponse))
-		                  		else
-		                    		ussdResponse = %{Message: "Invalid USSD request", Type: "Release"}
-		                    		send_resp(conn, :ok, Jason.encode!(ussdResponse))
-		                  		end
-			              	end
-			          	end
-      		end
-    	else
-      		ussdResponse = %{Message: "Invalid USSD request", Type: "Release"}
-      		send_resp(conn, :ok, Jason.encode!(ussdResponse))
-    	end
-  	end
+                    send_resp(conn, :ok, Jason.encode!(ussdResponse))
+                  else
+                    ussdResponse = %{Message: "Invalid USSD request", Type: "Release"}
+                    send_resp(conn, :ok, Jason.encode!(ussdResponse))
+                  end
+              end
+          end
+      end
+    else
+      ussdResponse = %{Message: "Invalid USSD request", Type: "Release"}
+      send_resp(conn, :ok, Jason.encode!(ussdResponse))
+    end
+  end
 end
