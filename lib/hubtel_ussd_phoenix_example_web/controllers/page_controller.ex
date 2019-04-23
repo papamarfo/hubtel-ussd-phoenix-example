d90e8707bcd3c43eab55f2beee075e314061aa87
Benjamin Manford <benjaminmanford@gmail.com>
2019-04-23 05:44:01 +0100

    Change variable names from camel case to snake case

--- lib/hubtel_ussd_phoenix_example_web/controllers/page_controller.ex
+++ lib/hubtel_ussd_phoenix_example_web/controllers/page_controller.ex
@@ -5,62 +5,62 @@ defmodule HubtelUssdPhoenixExampleWeb.PageController do
     # read raw POST data from Hubtel
     {:ok, body, _conn} = Plug.Conn.read_body(conn)
 
-    ussdRequest = Jason.decode!(body)
+    ussd_request = Jason.decode!(body)
 
-    # check if ussdRequest != nil
-    if ussdRequest do
+    # check if ussd_request != nil
+    if ussd_request do
       # get the request type
-      case ussdRequest["Type"] do
+      case ussd_request["Type"] do
         # initial request
         "Initiation" ->
-          ussdResponse = %{
+          ussd_response = %{
             Message:
-              "Welcome to AquaSafari (" <> ussdRequest["Mobile"] <> ")\n1. Continue\n2. Cancel",
+              "Welcome to AquaSafari (" <> ussd_request["Mobile"] <> ")\n1. Continue\n2. Cancel",
             ClientState: "continue-menu",
             Type: "Response"
           }
 
-          sendResponse(conn, :ok, ussdResponse)
+          send_response(conn, :ok, ussd_response)
 
         # continuing request
         "Response" ->
           # get request sequence
-          case ussdRequest["Sequence"] do
+          case ussd_request["Sequence"] do
             2 ->
               # get the clientState
-              case ussdRequest["ClientState"] do
+              case ussd_request["ClientState"] do
                 # if clientState == "continue-menu"
                 "continue-menu" ->
                   # check if user input is 1 from home menu
-                  if ussdRequest["Message"] == "1" do
-                    ussdResponse = %{
+                  if ussd_request["Message"] == "1" do
+                    ussd_response = %{
                       Message: "Select an option\n1. Book a Room\n2. Contact Us",
                       Type: "Release"
                     }
 
-                    sendResponse(conn, :ok, ussdResponse)
+                    send_response(conn, :ok, ussd_response)
                   else
-                    ussdResponse = %{
+                    ussd_response = %{
                       Message: "Invalid USSD request", 
                       Type: "Release"
                     }
 
-                    sendResponse(conn, :ok, ussdResponse)
+                    send_response(conn, :ok, ussd_response)
                   end
               end
           end
       end
     else
-      ussdResponse = %{
+      ussd_response = %{
         Message: "Invalid USSD request",
         Type: "Release"
       }
       
-      sendResponse(conn, :ok, ussdResponse)
+      send_response(conn, :ok, ussd_response)
     end
   end
 
-  def sendResponse(conn, :ok, ussdResponse) do
-    send_resp(conn, :ok, Jason.encode!(ussdResponse))
+  def send_response(conn, :ok, ussd_response) do
+    send_resp(conn, :ok, Jason.encode!(ussd_response))
   end
 end
