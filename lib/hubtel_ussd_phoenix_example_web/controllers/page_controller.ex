61bc68f2b45d587fcb8b73adae8ebcc01e8e3a44
Benjamin Manford <benjaminmanford@gmail.com>
2019-12-24 09:16:55 +0800

    Update page_controller.ex

--- lib/hubtel_ussd_phoenix_example_web/controllers/page_controller.ex
+++ lib/hubtel_ussd_phoenix_example_web/controllers/page_controller.ex
@@ -2,65 +2,61 @@ defmodule HubtelUssdPhoenixExampleWeb.PageController do
   use HubtelUssdPhoenixExampleWeb, :controller
 
   def index(conn, _params) do
-    # read raw POST data from Hubtel
+    # get request params
     {:ok, body, _conn} = Plug.Conn.read_body(conn)
+    request = Jason.decode!(body)
 
-    ussd_request = Jason.decode!(body)
+    if request do
+      case request["Type"] do
+        
+        "Initiation" -> # initial request
+          conn |> show_welcome_menu # show welcome menu
 
-    # check if ussd_request != nil
-    if ussd_request do
-      # get the request type
-      case ussd_request["Type"] do
-        # initial request
-        "Initiation" ->
-          ussd_response = %{
-            Message:
-              "Welcome to AquaSafari (" <> ussd_request["Mobile"] <> ")\n1. Continue\n2. Cancel",
-            ClientState: "continue-menu",
-            Type: "Response"
-          }
-
-          send_response(conn, :ok, ussd_response)
-
-        # continuing request
-        "Response" ->
-          # get request sequence
-          case ussd_request["Sequence"] do
+        "Response" -> # continuing request
+          case request["Sequence"] do
             2 ->
-              # get the clientState
-              case ussd_request["ClientState"] do
-                # if clientState == "continue-menu"
-                "continue-menu" ->
-                  # check if user input is 1 from home menu
-                  if ussd_request["Message"] == "1" do
-                    ussd_response = %{
-                      Message: "Select an option\n1. Book a Room\n2. Contact Us",
-                      Type: "Release"
-                    }
-
-                    send_response(conn, :ok, ussd_response)
-                  else
-                    ussd_response = %{
-                      Message: "Invalid USSD request", 
-                      Type: "Release"
-                    }
-
-                    send_response(conn, :ok, ussd_response)
+              case request["ClientState"] do
+                "last-menu" ->
+                  if request["Message"] == "1" do
+                    conn |> show_last_menu # show last menu
                   end
+                  
+                  conn |> show_invalid_selection # show invalid selection
               end
           end
       end
-    else
-      ussd_response = %{
-        Message: "Invalid USSD request",
-        Type: "Release"
-      }
-      
-      send_response(conn, :ok, ussd_response)
     end
+    
+    conn |> show_invalid_selection # show invalid selection
   end
 
-  def send_response(conn, :ok, ussd_response) do
-    send_resp(conn, :ok, Jason.encode!(ussd_response))
+  # show welcome menu
+  def show_welcome_menu(conn) do
+    response = %{
+      Message: "Welcome Message \n\n1. Continue\n2. Cancel",
+      ClientState: "last-menu",
+      Type: "Response"
+    }
+    send_response(conn, response)
+  end
+
+  # show last menu
+  def show_last_menu(conn) do
+    response = %{
+      Message: "Last Menu",
+      Type: "Release"
+    }
+    send_response(conn, response)
+  end
+
+  # show invalid selection menu
+  def show_invalid_selection(conn) do
+    response = %{Message: "Invalid selection", Type: "Release"}
+    send_response(conn, response)
+  end
+
+  # send response to service provider
+  def send_response(conn, response) do
+    send_resp(conn, :ok, Jason.encode!(response))
   end
 end
